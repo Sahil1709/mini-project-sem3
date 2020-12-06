87c3d373d75808e81a8d3fd77e106fe5573c775d
sahil <sahilofficial17@gmail.com>
2020-12-06 10:44:13 +0530

    footer

--- footer.php
+++ footer.php
@@ -11,25 +11,25 @@
 			<hr class=" col-xs-12" >
 			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 				<p style="font-weight:bolder;margin:3px;font-size:22px;color:white" >Sahil Gupta</p> 
-					Skills :<br> html,css,js,php,mysql<br>
-					Backend Developer
+					Skills :<br> javascript, php, mysql<br>
+					Backend/Database Developer
 			</div>
 			<hr class="visible-xs col-xs-12"></hr>
 			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 				<p style="font-weight:bolder;margin:3px;font-size:22px;color:white" >Vivek Pandey</p> 
-					Skills :<br> html,css,js,php,mysql<br>
-					Backend Developer
+					Skills :<br> html, css, bootstrap4<br>
+					Frontend Developer
 			</div>
 			<hr class="visible-xs col-xs-12"></hr>
 			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 				<p style="font-weight:bolder;margin:3px;font-size:22px;color:white" >Vikas Kumawat</p> 
-					Skills :<br> html,css,js,php,mysql<br>
-					Backend Developer
+					Skills :<br> html, css, scss<br>
+					Frontend Developer
 			</div>
 			<hr class="visible-xs col-xs-12"></hr>
 			<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 				<p style="font-weight:bolder;margin:3px;font-size:22px;color:white" >Mukul Verma</p> 
-					Skills :<br> html,css,js,php,mysql<br>
+					Skills :<br>javascript, php, mysqli<br>
 					Backend Developer
 			</div>
 			<hr class=" visible-xs col-xs-12"></hr>
